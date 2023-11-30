819db651a3b3a683e940be9194e3f159855f44da
Martin <martinmyhalyna@gmail.com>
2023-11-29 21:17:18 -0500

    task-04 finished

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener("input", (e) => {
-  if (input.value === "") {
+  if (input.value === "" || input.value === " ") {
     output.textContent = "Anonymous";
   } else {
     output.textContent = e.currentTarget.value;
--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,19 @@
+const registerForm = document.querySelector(".login-form");
+registerForm.addEventListener("submit", submitHandler);
 
+function submitHandler(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+  const trimmedValues = {
+    email: email.trim(),
+    password: password.trim(),
+  };
+  console.log(
+    `login: ${trimmedValues.email}, password: ${trimmedValues.password}`
+  );
+}
